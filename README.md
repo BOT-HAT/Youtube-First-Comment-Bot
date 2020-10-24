6dc7e85262fc2de04ef14d82979ac5af2c6b006e
Tiger Yu <tigeryu8900@gmail.com>
2020-10-24 13:39:50 +0800

    Update README.md, add gitignore, move config variables into config.json, add auto-detect last video, and add channel ID to playlist ID conversion logic

--- README.md
+++ README.md
@@ -1,29 +1,39 @@
 # Youtube-First-Comment-Bot
-A bot for automatically post comment when a youtube channel upload a new video.
-This bot checks a specified channel every specified intervel and if a new video found this bot will post a comment that you wants to post.
+A bot for automatically post comment when a youtube channel upload a new video. This bot checks a specified channel
+every certain amount of seconds, and if a new video is found, this bot will post a pre-defined comment on that video.
 
-Requirments
-python 2.7
-google api client modules
-google api console account
-Youtube account
+## Requirments
+- python version >= 3.5
+- [Google API Client (Python)](https://github.com/googleapis/google-api-python-client)
+- Google API Console project (create one [here](https://console.developers.google.com/))
+- YouTube account
 
-Usage
-Create an account and a api and a outh screen in google console
-Replace the file "client_secrets.json" with the downloaded Credentials json file from google console
-Edit these variables in bot.py
-  cid="UUX6OQ3DkcsbYNE6v8uQQuVA" puth uploads id of the required channel here. (
-  lastvid="Z9WQy9uEY8M"  put last video id here 
-  intervel=5 put the time waiting intervel here
-  comment="Put your text here"  edit this variable amnd put the required comment here.
-Remove contents in "bot.py-oauth2.json" before usage
-if you setup everything properly the bot will ask you to goto a link from your browser and login with your youtube account ( this is a one time only requirment)
+## Setup
+Go to [Google API Console](https://console.developers.google.com/) to create an account (if you haven't) and create a
+project. Remember to enable YouTube Data API for that project. Add an OAuth Client ID from "Create credentials" and
+choose "Desktop app" from the drop-down menu on the next screen. After the client ID is created, download it by clicking
+on the download button. Rename the file to `client_secret.json` and place it in the project's root directory, which is
+in the same folder as this readme. Then, create a virtualenv environment and install google-api-python-client and
+oauth2client in this project's root directory. Finally, change the values in `config.json`. `id` is the channel id or
+playlist id, `interval` is the number of seconds between each query (put a number here, the string is just a
+placeholder), and `comment` is the comment to be posted.
 
+## Running
+Run `bot.py` with the virtualenv you've created. If this is the first time the program is run, or if the
+`bot.py-oauth2.json` file is missing or empty, you will be prompted to login. You can always delete or clear that file
+before running to log into another account. The script will then query YouTube API to retrieve the last posted video,
+and then it will query every certain number of seconds (defined in `config.json`) to check whether there's a new video,
+and if there is a new video, it will comment, and the program will terminate.
 
-Helps
-How to find upload it? see this video https://www.youtube.com/watch?v=RjUlmco7v2M .
-To find last video go the the required channel and check the url of the latest upload an url may look like this https://www.youtube.com/watch?v=RjUlmco7v2M take the last string that is "RjUlmco7v2M" this is video id.
-To set up a good time intervel you must consider several things eg youtube api quata limit system resources etc... if you planed to run this bot 24x7 then put the inyer vell as 20 to 25 seconds, if you are running this bot only for one or two hour you can put this intervel as 1 or 2 seconds. this is to prevent youtube api limit.
+## Help
+How to find video ID? see [this video](https://www.youtube.com/watch?v=RjUlmco7v2M).
+To find last video, go the YouTube channel and check the URL of the latest upload. Using this URL
+https://www.youtube.com/watch?v=RjUlmco7v2M for example, and the `v` parameter is the video ID (in this case, it is
+"RjUlmco7v2M"). To set up a good time interval you must consider several things eg youtube API quata limit system
+resources etc... If you are planning to run this bot 24/7, then set the interval between 20 and 25 seconds, and if you
+are running this bot only for one or two hours, you can set this interval to 1 or 2 seconds. This is to prevent reaching
+the API limit.
 
 Note.
-Feel free to modify i have messed up with arguments in the last few section, the code still works but there is a lot of room for improvements.
+Feel free to modify. I have messed up with arguments in the last few section, the code still works but there is a lot of
+room for improvements.
--- bot.py
+++ bot.py
@@ -2,91 +2,98 @@ import httplib2
 import os
 import sys
 import time
+import json
+from operator import itemgetter
 
-from apiclient.discovery import build_from_document
-from apiclient.errors import HttpError
+from googleapiclient.discovery import build_from_document
+from googleapiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 
-# ======== Configure the following variables ===========
-#uploads id
-cid="UUX6OQ3DkcsbYNE6v8uQQuVA"
-#last video id wMuYiLby3-s
-lastvid="Z9WQy9uEY8M"
-# waiting time intervel in seconds
-intervel=5
-#comment you need to post
-comment="Put your text here"
+# Configure these variables in config.json
+id, interval, comment = itemgetter("id", "interval", "comment")(json.load(open("./config.json", "r")))
 
-CLIENT_SECRETS_FILE = "./client_secrets.json"
+CLIENT_SECRETS_FILE = "client_secret.json"
 YOUTUBE_READ_WRITE_SSL_SCOPE = "https://www.googleapis.com/auth/youtube.force-ssl"
-YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_SERVICE_NAME = "service"
 YOUTUBE_API_VERSION = "v3"
 MISSING_CLIENT_SECRETS_MESSAGE = """
 WARNING: Please configure OAuth 2.0
-To make this sample run you will need to populate the client_secrets.json file
+To make this sample run you will need to populate the client_secret.json file
 found at:
    %s
 with information from the APIs Console
 https://console.developers.google.com
-For more information about the client_secrets.json file format, please visit:
+For more information about the client_secret.json file format, please visit:
 https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                    CLIENT_SECRETS_FILE))
-def get_authenticated_service(args):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE,
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+# converts id to playlistId and store in pid
+pid = "UU" + id[2:] if id[:2] == "UC" else id
+print "id:", id + ",", "pid:", pid
 
-  storage = Storage("%s-oauth2.json" % sys.argv[0])
-  credentials = storage.get()
+def get_authenticated_service(flags):
+    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE,
+                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-  if credentials is None or credentials.invalid:
-    credentials = run_flow(flow, storage, args)
-  with open("youtube-v3-discoverydocument.json", "r") as f:
-    doc = f.read()
-    return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
+    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        credentials = run_flow(flow, storage, flags)
+    with open("youtube-v3-discoverydocument.json", "r") as f:
+        doc = f.read()
+        return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
+
+
 def insert_comment(youtube, parent_id, text):
-  insert_result = youtube.commentThreads().insert(
-  part="snippet",
-  body={
-    "snippet": {
-      "videoId": parent_id,
-      "topLevelComment": {
-        "snippet": {
-          "textOriginal": text
-          }
+    insert_result = youtube.commentThreads().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "videoId": parent_id,
+                "topLevelComment": {
+                    "snippet": {
+                        "textOriginal": text
+                    }
+                }
+            }
         }
-      }
-    }
-  )
-  response = insert_result.execute()
-  print ("comment added")
-def lastvideo(youtube, cid):
-  request = youtube.playlistItems().list(
-    part="snippet",
-    playlistId=cid
     )
-  response = request.execute()
-  return(response["items"][0]["snippet"]["resourceId"]["videoId"])
+    response = insert_result.execute()
+    print ("comment added")
+
+
+def get_last_video(youtube, pid):
+    request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=pid
+    )
+    response = request.execute()
+    return response["items"][0]["snippet"]["resourceId"]["videoId"]
+
+
 argparser.add_argument("--text", help="Required; text that will be used as comment.")
-args = argparser.parse_args()
-args.videoid=lastvid
-args.text=comment
-youtube = get_authenticated_service(args)
-i=0
+flags = argparser.parse_args()
+flags.text = comment
+youtube = get_authenticated_service(flags)
+last_video = get_last_video(youtube, pid)
+print "last_video:", last_video
+
+i = 0
 while True:
-    last=lastvideo(youtube,cid)
-    i=i+1
-    if(last!=lastvid):
-      print(last)
-      try:
-        insert_comment(youtube, last, args.text)
-      except HttpError, e:
-        print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
-      else:
-        print "Comment Inserted"
-        break
-    time.sleep(intervel)
+    last = get_last_video(youtube, pid)
+    i += 1
+    if last != last_video:
+        print(last)
+        try:
+            insert_comment(youtube, last, flags.text)
+        except HttpError, e:
+            print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+        else:
+            print "Comment Inserted"
+            break
+    time.sleep(interval)
     print("waiting......")
     print "Cycle:", i
